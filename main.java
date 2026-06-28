be7f4eac090c34fb2b9c7508d843eebf6366bfc9
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-06-28 12:09:36 +0530

    Day 1: Java Core Primitive and type casting completed

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {   
+        // primitive data types
+        byte age = 23;
+        int phn = 1478523690;
+        long phone = 12121212513L;
+        float pi = 3.14F;
+        boolean isSlacking = false;
+        String performance = "a";
+        
+        // Type Casting
+        double price = 55.99;        
+        int roundedPrice = (int) price;
+
+        // Printing values
+        System.out.println("Original Price " + price);
+        System.out.println("Converted price " + roundedPrice);
+
+
+    }
+}
