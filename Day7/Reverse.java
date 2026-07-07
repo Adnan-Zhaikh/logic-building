155b6399cd160a884e077b853c5a4d8cf64e314b
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-07 10:52:47 +0530

    D7: Reverse array using loop and pointers

--- Day7/Reverse.java
+++ Day7/Reverse.java
@@ -1,11 +1,23 @@
 package Day7;
 
+import java.util.Arrays;
+
 public class Reverse {
     public static void main(String[] args) {
         int[] data = {10,20,30,40,50,60};
 
         int left = 0, right = data.length -1;
 
-        
-    }
+        while ( left < right ) {
+            int temp = data[left];
+            data[left] = data[right];
+            
+            data[right] = temp;
+
+            left++;
+            right--;
+        }
+        System.out.println("Array after reverse: "+ Arrays.toString(data));
+            
+ }
 }
