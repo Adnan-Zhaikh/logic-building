fd558a64f9ab2cbd91def66f0774ed5547f73992
Adnan-Zhaikh <adnanibrahimshaikh@gmail.com>
2026-07-11 22:41:21 +0530

    Binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,23 @@
-public BinarySearch {
-    
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] num = {10,20,30,40,50,60,70};
+        int target = 50;
+
+        int left = 0;
+        int right = num.length - 1;
+        int foundIndex = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (num[mid] == target) {
+                foundIndex = mid;
+                break;
+            } else if (num[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(foundIndex);       
+    }
 }
